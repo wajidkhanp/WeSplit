c830c44a9faf6baa75914a3683c75f61ec9eebc6
wajid <wajid@live.ca>
2020-07-05 16:01:15 -0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Wajid K Patan on 7/5/20.
+//  Copyright © 2020 wajidInc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
