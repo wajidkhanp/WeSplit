82b03fff7f3b1298ef2b282f47040572f6378821
wajid <wajid@live.ca>
2020-07-05 17:38:24 -0700

    WeSplit n this project you learn about the basic structure of SwiftUI apps, how to build forms and sections, creating navigation views and navigation bar titles, how to store program state with the @State property wrapper, how to create user interface controls like TextField and Picker, and how to create views in a loop using ForEach. Even better, you have a real project to show off for your efforts.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  
+  let tipPercentages = [ 5, 10, 15, 20, 25, 0]
+  
+  var totalPerPerson: Double {
+    let peopelCount = Double(numberOfPeople+2)
+    let tipSelection = Double(tipPercentages[tipPercentage])
+    let orderAmount = Double(checkAmount) ?? 0
+    
+    return (orderAmount + (orderAmount * (tipSelection/100)))/peopelCount
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", text: $checkAmount)
+            .keyboardType(.decimalPad)
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2..<100){
+              Text("\($0) People" )
+            }
+          }
+        }
+        
+        Section(header: Text("How much tip you want to leave")) {
+          Picker("Tip Percentage", selection: $tipPercentage) {
+            ForEach(0..<tipPercentages.count) {
+              Text("\(self.tipPercentages[$0])%")
+            }
+          }.pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Section(header: Text("Total per person is")){
+          Text("$\(totalPerPerson, specifier: "%.2f")")
+        }
+      }.navigationBarTitle("WeSplit")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
